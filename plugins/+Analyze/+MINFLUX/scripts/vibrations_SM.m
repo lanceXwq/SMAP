695453591c7107a4cbd943044be144ac713b43ca
Jonas Ries <ries@a743-rie-mac2.msp.univie.ac.at>
2025-10-30 10:39:28 +0100

    minflux vibration analysis

--- plugins/+Analyze/+MINFLUX/scripts/vibrations_SM.m
+++ plugins/+Analyze/+MINFLUX/scripts/vibrations_SM.m
@@ -1,57 +1,55 @@
 % calculate average FFT from FoV and other measures
 % we expect constant dt, so low photon threshold
 % parameters
-minlen=5000; %minimum number of localizations in track
-maxfreq=350;
-skipfirst=5;
-dfreq=0.1;
+p.minlen=5000; %minimum number of localizations in track
+p.maxfreq=350;
+p.skipfirst=5;
+p.dfreq=0.1;
 
+figure(88); clf
+ax1=subplot(2,1,1);
+plotfft("xnm",p,g,ax1);
+ax2=subplot(2,1,2);
+plotfft("ynm",p,g,ax2);
+
+function [xfp,freq,peaks]=plotfft(field,p,g,ax)
+if nargin<4
+    ax=gca;
+end
 usefields={'xnm','ynm','znm','time','groupindex','numberInGroup','filenumber','efo','cfr','eco','ecc','efc','tid','fbg','phot'};
 locs=g.locData.getloc(usefields,'layer',find(g.getPar('sr_layerson')),'Position','all','removeFilter',{'filenumber','time'});
 mtid=max(double(locs.tid));
 tidf=double(locs.tid)+double(locs.filenumber)*mtid;
-indlong=locs.numberInGroup>=minlen;
+indlong=locs.numberInGroup>=p.minlen;
 trackids=unique(tidf(indlong));
-figure(88); clf
-ax1=subplot(2,1,1);
-ax2=subplot(2,1,2);
-% dfreq=1/min(diff(locs.time(indlong)))
-fall=0:dfreq:maxfreq;
-ftxa=0;ftya=0;
+fall=0:p.dfreq:p.maxfreq;
+ftxa=0;
 
-
+x=locs.(field);t=locs.time;
 for k=1:length(trackids)
     ig=tidf==trackids(k);
     igf=find(ig);
-    [yfp,freq]=getfft(locs.ynm(igf(skipfirst:end)),locs.time(igf(skipfirst:end)));
-    
-    yfpbh=bindata(freq,yfp,fall);
-    plot(ax2,fall,yfpbh,'c'); hold(ax1,'on') 
-    yfpbh(isnan(yfpbh))=0;
-    ftya=yfpbh*sum(ig)+ftya;
-    % end
-
-    [xfp,freq]=getfft(locs.xnm(igf(skipfirst:end)),locs.time(igf(skipfirst:end)));
+    [xfp,freq]=getfft(x(igf(p.skipfirst:end)),t(igf(p.skipfirst:end)));
     xfpbh=bindata(freq,xfp,fall);
-    plot(ax1,fall,xfpbh,'c'); hold(ax2,'on') 
+    plot(ax,fall,xfpbh,'c'); hold(ax,'on') 
     xfpbh(isnan(xfpbh))=0;
     ftxa=xfpbh*sum(ig)+ftxa; 
-    % end
 end
 % ftxa=ftxa/length(trackids);
 ftxa=ftxa/length(ig);
-ftya=ftya/length(ig);
-plot(ax1,fall,ftxa,'k','LineWidth',2);
-xlabel(ax1,'frequency (Hz)')
-ylabel(ax1,'Amplitude (nm), 2*fft (x)');
-axis(ax1,'tight')
-ax1.YLim(2)=0.25;
+plot(ax,fall,ftxa,'k','LineWidth',2);
+xlabel(ax,'frequency (Hz)')
+ylabel(ax,'Amplitude (nm), 2*fft (x)');
+axis(ax,'tight')
+ax.YLim(2)=0.25;
 
-plot(ax2,fall,ftya,'k','LineWidth',2);
-xlabel(ax2,'frequency (Hz)')
-ylabel(ax2,'Amplitude (nm), 2*fft (x)');
-axis(ax2,'tight')
-ax2.YLim(2)=0.25;
+[pks,pkind]=findpeaks(ftxa,"MinPeakHeight",2*mean(ftxa),"MinPeakDistance",5);
+plot(fall(pkind),pks,'ro')
+for k=1:length(pks)
+    text(fall(pkind(k)),pks(k),string(fall(pkind(k))))
+end
+peaks=horzcat(fall(pkind)',pks');
+end
 
 function [xfp,freq]=getfft(x,t)
 dt=mode(diff(t));
